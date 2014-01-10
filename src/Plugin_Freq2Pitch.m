8f39fcb003ac234bd22ec7a20c36efdcb94a4a07
tuxzz <dorazzsoft@gmail.com>
2014-01-10 10:35:14 +0800

    Some changes of Plugin_Freq2Pitch.m

--- src/Plugin_Freq2Pitch.m
+++ src/Plugin_Freq2Pitch.m
@@ -1,12 +1,12 @@
-function FreqToPitch(freq)
-  FreqToPitch=69+12*log2(freq/440)
+function [ret]=Plugin_Freq2Pitch(freq)
+  ret=69+12*log2(freq/440)
 end
 
 %	C C#(Db) D D#(Eb) E F F#(Gb) G G#(Ab) A A#(Bb) B
-function pp(pitch)
+function [ret]=Plugin_Pitch2Shierlv(pitch)
   shierlv=["C";"C#(Db)";"D";"D#(Eb)";"E";"F";"F#(Gb)";"G";"G#(Ab)";"A";"A#(Bb)";"B";]
   s=fix(pitch/12)
   d=pitch-s*12
   id=round(d)+1
-  pp=strcat(shierlv(id,:),mat2str(s-1),"    error:",mat2str(-((id-d-1)*100)),"%")
+  ret=strcat(shierlv(id,:),mat2str(s-1),"    error:",mat2str(-((id-d-1)*100)),"%")
 end
